d2a064e9b214eceddbedeff411713c68e00956d7
Zamzam <Zamzamhassan44@gmail.com>
2022-11-20 15:19:03 +0300

    installed react router dom and wrote function for all the components

---
+++ src/components/ContactForm.js
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+      <div>
+        <h1>This is the home page</h1>
+      </div>
+    );
+  }
+  
+  export default Contact;
---
+++ src/components/Hero.js
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+      <div>
+        <h1>This is the home page</h1>
+      </div>
+    );
+  }
+  
+  export default Hero;
