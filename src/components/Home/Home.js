3f50e4c32dc7939c33f520d1e2bbb8067911a57d
Zamzam <Zamzamhassan44@gmail.com>
2022-11-21 16:10:52 +0300

    added link from homepage to heros page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,18 @@
 import './Home.css'
+import { Link, NavLink } from "react-router-dom"
+
 
 function Home() {
   return (
-    <div style={{ 
-      backgroundImage: `url("https://i.pinimg.com/564x/82/36/3b/82363bb444435cb16107036214a51294.jpg")`
-    }}>
-      Hello World
+    <div 
+    style={{ 
+      backgroundImage: `url("https://i.pinimg.com/564x/ae/dc/3e/aedc3e7ca11289982fc2de972fbe6176.jpg")`
+    }} className="image"> 
+        <h1 className='heading'>Magical kenya</h1>
+        <NavLink to='/hero'>
+        <img src='https://i.pinimg.com/236x/46/9e/dc/469edc56a2cd471e46d41cdb06d66b8b.jpg' text-center className='profile'></img>
+
+        </NavLink>
     </div>
   );  
   
