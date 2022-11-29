a9aef9f06b75ad011ed979de095b84792e0ee59c
Zamzam <Zamzamhassan44@gmail.com>
2022-11-30 00:22:30 +0300

    lamu.js

--- src/components/Lamu/Lamu.js
+++ src/components/Lamu/Lamu.js
@@ -6,92 +6,60 @@ const Lamu = () => {
         <div>
         <h1>Lamu Island</h1>
             
-<p>Brimming with intriguing history, natural beauty, and amenities for every travel style, but set apart from the tourist crowds? Then Lamu Island certainly deserves a close look!
-
-Located a mere two hours by air from bustling Nairobi, arriving on the UNESCO-designated island may feel like you’ve journeyed to the edge of the world – and back in time as well.
-
-Cars? Not on Lamu Island. Instead, expect to find plenty of donkeys plying the narrow alleyways of this ancient settlement.
-
-International restaurant and hotel chains? Nope. But you will find a wide range of accommodation, from basic homestay-style guesthouses to luxury boutique hotels just steps from white sands beaches – plus, a sumptuous mix of Swahili and other regional cuisine and locally-owned international-style cafes, bars, and restaurants.
-
-And that’s just the beginning!</p>
-<h2 className='text-center'>Reasons To Go To Lamu Island</h2>
+        <p>Brimming with intriguing history, natural beauty, and amenities for every travel style, but set apart from the tourist crowds? Then Lamu Island certainly deserves a close look!
+           Located a mere two hours by air from bustling Nairobi, arriving on the UNESCO-designated island may feel like you’ve journeyed to the edge of the world – and back in time as well.
+           Cars? Not on Lamu Island. Instead, expect to find plenty of donkeys plying the narrow alleyways of this ancient settlement.
+           International restaurant and hotel chains? Nope. But you will find a wide range of accommodation, from basic homestay-style guesthouses to luxury boutique hotels just steps from white sands beaches – plus, a sumptuous mix of Swahili and other regional cuisine and locally-owned international-style cafes, bars, and restaurants.
+           And that’s just the beginning!</p>
+        
+        <h2 className='text-center'>Reasons To Go To Lamu Island</h2>
         <hr></hr>
+
         <h1>The Ancient Alleyways of Lamu Old Town</h1>
         <img id="img1" src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0c/3e/5c/8c/photo7jpg.jpg?w=1200&h=1200&s=1" alt="" />
         <p>With its seemingly endless stretch of golden sands and tasteful assortment of coral heritage guesthouses, portions of Lamu Island (particularly Shela Village and the south coast) evoke a beach-lover’s paradise.
-
-        But Lamu Island is so much more than an East Africa beach destination!
-
-        At the heart of this stunning and largely uninhabited island is the extraordinarily atmospheric ancient Swahili settlement of Lamu Town (aka Old Town).
-
-        Move over Zanzibar Stone Town, Lamu Old Town is the oldest and best-preserved Swahili settlement in all of East Africa, having been a key settlement for trade since the 14th century.
-
-        Designated a UNESCO World Heritage Site in 2001, this town of coral stone and timber, with its vast network of narrow alleyways, evokes the important Swahili trade settlement of centuries past.
-
-        Be sure to save plenty of time for wandering historic Old Town, exploring its evocative historic sites and interesting shops and galleries, chatting with quirky and insightful local residents, and admiring the many striking mosques (there are around 40 of them on this small island!).
-
-        After nightfall, Old Town comes alive, as residents spill out from their homes to enjoy the lovely Kenyan Coast evening. Don’t miss a stroll around during this part of the day for a true slice of everyday life on Lamu Island.</p>
+           But Lamu Island is so much more than an East Africa beach destination!
+           At the heart of this stunning and largely uninhabited island is the extraordinarily atmospheric ancient Swahili settlement of Lamu Town (aka Old Town).
+           Move over Zanzibar Stone Town, Lamu Old Town is the oldest and best-preserved Swahili settlement in all of East Africa, having been a key settlement for trade since the 14th century.
+           Designated a UNESCO World Heritage Site in 2001, this town of coral stone and timber, with its vast network of narrow alleyways, evokes the important Swahili trade settlement of centuries past.
+           Be sure to save plenty of time for wandering historic Old Town, exploring its evocative historic sites and interesting shops and galleries, chatting with quirky and insightful local residents, and admiring the many striking mosques (there are around 40 of them on this small island!).
+           After nightfall, Old Town comes alive, as residents spill out from their homes to enjoy the lovely Kenyan Coast evening. Don’t miss a stroll around during this part of the day for a true slice of everyday life on Lamu Island.</p>
         
    
-   <h1>Staying in a Swahili Coral Mansion</h1>
-
+        <h1>Staying in a Swahili Coral Mansion</h1>
         <img id="img2" src="https://www.re-thinkingthefuture.com/wp-content/uploads/2021/09/A5236-An-overview-of-Swahili-Architecture-Image-3.jpg" alt=""/>
         <p>The number of excellent lodging options on Lamu Island has grown in the past decade, and many older guesthouses continue to tastefully renovate and improve their offerings and amenities for guests.
-
-While there are numerous lodging options at nearly every price point, historic Swahili mansions converted into boutique guesthouses offer the quintessential Lamu stay that many visitors come here hoping to find.
-
-In many cases, these guesthouses are carefully preserved and painstakingly refurbished former residences of the island’s former elite, originally constructed of blocks hewn from coral.</p>
-    </div>   
+           While there are numerous lodging options at nearly every price point, historic Swahili mansions converted into boutique guesthouses offer the quintessential Lamu stay that many visitors come here hoping to find.
+           In many cases, these guesthouses are carefully preserved and painstakingly refurbished former residences of the island’s former elite, originally constructed of blocks hewn from coral.</p>
 
         <h1>Swahili Coastal Cuisine</h1>
         <img id="img1" src="https://i.pinimg.com/564x/20/83/fe/2083fee6910871b174eb5a72882bfdb0.jpg" alt="" />
         <p>An eclectic and satisfying mix of dining and drinking options is yet another reason excellent reason to visit Lamu Island.
-
-No doubt, Swahili cuisine is king here, and you generally can’t go wrong ordering up the specialty of the day from a crowded local eatery.
-
-But the island also hosts several excellent cafes and restaurants serving German, Swedish, Mediterranean, French, Italian, and even Bosnian cuisine – and more!
-
-Historic Lamu and sleepy Shela also have small, low-key pockets of nightlife. But do note, this is a predominately Muslim community, so you won’t find as many cafes and eateries serving alcohol as you might find elsewhere in Kenya.</p>
+           No doubt, Swahili cuisine is king here, and you generally can’t go wrong ordering up the specialty of the day from a crowded local eatery.
+           But the island also hosts several excellent cafes and restaurants serving German, Swedish, Mediterranean, French, Italian, and even Bosnian cuisine – and more!
+           Historic Lamu and sleepy Shela also have small, low-key pockets of nightlife. But do note, this is a predominately Muslim community, so you won’t find as many cafes and eateries serving alcohol as you might find elsewhere in Kenya.
+           Here are just a few suggestions of places to try while you are on Lamu Island:
+           LOCAL (SWAHILI) CUISINE ▸ For popular Swahili offerings and local favorites in Old Town, don’t miss Seafront Cafe (on the waterfront near Lamu Fort), Mangrove Restaurant (near the Museum Jetty) and Umalila (next to Lamu House Hotel). Stop Over (along the Corniche Path, halfway to Shela) is one of our personal favorites. For something completely different, don’t miss dinner or a drink on the Floating Restaurant (located in the channel between the Museum Jetty and hospital).
+           INTERNATIONAL CUISINE ▸ If you’re in the mood for a taste of home (or something a bit more exotic, depending on where you are from), check out Whispers Coffeeshop for “Western” delights and Moonrise Restaurant (formerly Lamu Beach Club) in Lamu House Hotel. In Shela Village, Peponi Hotel Restaurant for excellent European and seafood dishes with seafront seating, and Kijani Rooftop Restaurant for excellent international cuisine with a view.</p>
        
-       <p>Here are just a few suggestions of places to try while you are on Lamu Island:
-
-LOCAL (SWAHILI) CUISINE ▸ For popular Swahili offerings and local favorites in Old Town, don’t miss Seafront Cafe (on the waterfront near Lamu Fort), Mangrove Restaurant (near the Museum Jetty) and Umalila (next to Lamu House Hotel). Stop Over (along the Corniche Path, halfway to Shela) is one of our personal favorites. For something completely different, don’t miss dinner or a drink on the Floating Restaurant (located in the channel between the Museum Jetty and hospital).
-
-INTERNATIONAL CUISINE ▸ If you’re in the mood for a taste of home (or something a bit more exotic, depending on where you are from), check out Whispers Coffeeshop for “Western” delights and Moonrise Restaurant (formerly Lamu Beach Club) in Lamu House Hotel. In Shela Village, Peponi Hotel Restaurant for excellent European and seafood dishes with seafront seating, and Kijani Rooftop Restaurant for excellent international cuisine with a view.</p>
-       
-        <h1>Historic Landmarks & Points of Interest
-</h1>
+        <h1>Historic Landmarks & Points of Interest</h1>
         <img id="img2" src="https://i.ytimg.com/vi/iuLnY5AKo7k/maxresdefault.jpg" alt=""/>
         <p>There are several historic landmarks and museums worth seeing on the island and beyond.
-
-Arabs, Persians, Portuguese, Chinese, Germans, British and Bantu Africans have all made indelible marks on Lamu, which is reflected in the island’s culture and architecture.
-
-The primary religion is overwhelmingly Muslim — there are over 40 mosques on the small island! (nearly a dozen in the town center — which becomes sort of a “Battle of the Mosques” at prayer time, with the various Muezzins all calling out over the loudspeakers simultaneously (sometimes even at 4am!).
-
-A few points of interest that should be on your short list of places to visit in Lamu Island out are:
-
-LAMU WATERFRONT ▸ The Old Town waterfront has changed little in the past century, particularly the stretch of the Corniche Path just north and south of the Museum Jetty. This is one of the best places to get a sense of what this trading settlement might have been like in its heyday on a leisurely sunrise/sunset stroll.
-
-LAMU FORT ▸ Lamu Fort has long been at the center of daily life in Lamu since its construction in the early 19th century. It was here that the Sultan of Oman consolidated his power in East Africa, greatly increasing Lamu’s prominence before its eventual decline following the Sultan’s relocation to Zanzibar. The Fort served as a prison through much of the 20th century, and currently serves as a cultural exhibition hall and museum.
-
-RIYADHA MOSQUE ▸ Dating to 1900, Riyadha Mosque is one of the most prominent in town for its esteemed Centre for Islamic Studies and for hosting the annual Maulidi Festival – where pilgrims come from across East Africa to join in prayers and compete in donkey races and dhow-sailing events.
-
-LAMU DONKEY SANCTUARY ▸ By some estimates, there are as many as 6,000 donkeys on Lamu Island. Yet, until fairly recently there were few services on the island to care for the health and wellbeing of Lamu’s beloved workhorses…or work-donkeys, so to speak. In 1987, that changed with the opening of the Donkey Sanctuary, which provides free treatment to the island’s donkeys.
-
-THE FORT OF SHELA ▸ At the water line just south of Shela Village, one might easily mistake the imposing garrison-like structure perched on the point as a historic Omani stronghold. But in fact the impressive Fort of Shela is a much more contemporary structure (c. 2001) dreamed up by an Italian aristocrat. Following his death, his wife transformed their incredible home into a luxury hotel.</p>
+           Arabs, Persians, Portuguese, Chinese, Germans, British and Bantu Africans have all made indelible marks on Lamu, which is reflected in the island’s culture and architecture.
+           The primary religion is overwhelmingly Muslim — there are over 40 mosques on the small island! (nearly a dozen in the town center — which becomes sort of a “Battle of the Mosques” at prayer time, with the various Muezzins all calling out over the loudspeakers simultaneously (sometimes even at 4am!).
+           A few points of interest that should be on your short list of places to visit in Lamu Island out are:
+           LAMU WATERFRONT ▸ The Old Town waterfront has changed little in the past century, particularly the stretch of the Corniche Path just north and south of the Museum Jetty. This is one of the best places to get a sense of what this trading settlement might have been like in its heyday on a leisurely sunrise/sunset stroll.
+           LAMU FORT ▸ Lamu Fort has long been at the center of daily life in Lamu since its construction in the early 19th century. It was here that the Sultan of Oman consolidated his power in East Africa, greatly increasing Lamu’s prominence before its eventual decline following the Sultan’s relocation to Zanzibar. The Fort served as a prison through much of the 20th century, and currently serves as a cultural exhibition hall and museum.
+           RIYADHA MOSQUE ▸ Dating to 1900, Riyadha Mosque is one of the most prominent in town for its esteemed Centre for Islamic Studies and for hosting the annual Maulidi Festival – where pilgrims come from across East Africa to join in prayers and compete in donkey races and dhow-sailing events.
+           LAMU DONKEY SANCTUARY ▸ By some estimates, there are as many as 6,000 donkeys on Lamu Island. Yet, until fairly recently there were few services on the island to care for the health and wellbeing of Lamu’s beloved workhorses…or work-donkeys, so to speak. In 1987, that changed with the opening of the Donkey Sanctuary, which provides free treatment to the island’s donkeys.
+           THE FORT OF SHELA ▸ At the water line just south of Shela Village, one might easily mistake the imposing garrison-like structure perched on the point as a historic Omani stronghold. But in fact the impressive Fort of Shela is a much more contemporary structure (c. 2001) dreamed up by an Italian aristocrat. Following his death, his wife transformed their incredible home into a luxury hotel.</p>
        
-<h1>Lamu Fort</h1>
+        <h1>Lamu Fort</h1>
         <img id="img1" src="https://www.islamicarchitecturalheritage.com/wp-content/uploads/2020/07/lamu-fort.jpg" alt="" />
-<p>
-The buildings of Lamu Old Town, the oldest and best-preserved Swahili settlement in East Africa, are constructed in coral stone and mangrove timber. The town is located on the island of Lamu, on the African East coast, some 250km north of Mombasa. Founded in or before the 14th century, like Kilwa, Zanzibar and other Swahili ports, a trading centre with a prominent role in the slavetrade. Lamu Old Town has maintained much of its traditional atmosphere with narrow streets and Swahili architecture which combines Swahili, Arabic, Persian, Indian and European building elements.
-
-The Lamu Fort, built between 1813-1821 by Omanis, served to protect Omani interest along the East African coast. From 1910 to 1984 it was used as a prison. Today Lamu Island has become an important centre for the study of Swahili and Islamic cultures. Lamu Old Town is a UNESCO World Heritage Site.
-
-The Zamani Project team visited Lamu Island on a field campaign in 2006 and spatially documented Lamu Fort, as well as the Swahili House Museum and the nearby Shela Mosque.
-
-The Zamani Project team visited Lamu Island on a field campaign in 2006, and spatially documented Lamu Fort, as well as the Swahili House Museum and the nearby Shela Mosque.  
-</p>
+        <p>he buildings of Lamu Old Town, the oldest and best-preserved Swahili settlement in East Africa, are constructed in coral stone and mangrove timber. The town is located on the island of Lamu, on the African East coast, some 250km north of Mombasa. Founded in or before the 14th century, like Kilwa, Zanzibar and other Swahili ports, a trading centre with a prominent role in the slavetrade. Lamu Old Town has maintained much of its traditional atmosphere with narrow streets and Swahili architecture which combines Swahili, Arabic, Persian, Indian and European building elements.
+           The Lamu Fort, built between 1813-1821 by Omanis, served to protect Omani interest along the East African coast. From 1910 to 1984 it was used as a prison. Today Lamu Island has become an important centre for the study of Swahili and Islamic cultures. Lamu Old Town is a UNESCO World Heritage Site.
+           The Zamani Project team visited Lamu Island on a field campaign in 2006 and spatially documented Lamu Fort, as well as the Swahili House Museum and the nearby Shela Mosque.
+           The Zamani Project team visited Lamu Island on a field campaign in 2006, and spatially documented Lamu Fort, as well as the Swahili House Museum and the nearby Shela Mosque. </p>
        
      
    
@@ -133,7 +101,9 @@ You will find dream beaches in the north of Manda Toto on both sides of the smal
 
 
 </p>
-    </div>    
+    </div>   
+    </div>   
+ 
          );
 }
  
