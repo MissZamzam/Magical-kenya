ea80a184c0b44fa71e252c70005e7afd3f5b4820
Zamzam <Zamzamhassan44@gmail.com>
2022-12-13 22:40:05 +0300

    made changes to Diani.js

--- src/components/Diani/Diani.js
+++ src/components/Diani/Diani.js
@@ -7,21 +7,46 @@ const Diani = () => {
         <h1>Diani Beach</h1>
         <hr></hr>
 
-        <h1>Wrap Text</h1>
+        <h1>Skydiving in Diani</h1>
         <img id="img1" src="https://i.pinimg.com/236x/85/63/17/856317b3f208de8ecf37c85840747285.jpg" alt="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.</p>
-       
-        <h1>Wrap Text</h1>
+        <p>Skydiving at Diani is a popular attraction for people all over the world and the ever-clear skies make the sport extremely enjoyable.
+
+            Diani is home to Skydiving Diani, Kenya’s only permanent Skydiving club. The club organizes tandem skydiving activities and quality equipment for safe jumping.
+
+            The skydiving clubs give you quality jumping gear and a qualified guide for about $350.
+
+            Every March and November, Skydive Diani organizes the weekly boogies festivals. During the event, tourists get a chance to enjoy advanced skydiving antiques and an exclusive training session.
+
+            To make your experience even more exhilarating, Skydive Diani offers the off-planes package, where divers jump from an airplane, of course with the help of a guide.</p>
+        <h1>Scuba Diving, Snorkeling, and Boat Trips </h1>
         <img id="img2" src="https://i.pinimg.com/236x/85/63/17/856317b3f208de8ecf37c85840747285.jpg" alt=""/>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.</p>
-    
-        <h1>Wrap Text</h1>
+        <p>To explore the richness and diversity of marine life at Diani Beach, you can go on boat trips, snorkeling or do scuba diving.
+
+            These are not only some of the most fun activities in Diani, but also they provide a forum to interact closely with marine life.
+
+            Imagine an encounter with the whale shark? Swimming in the same waters with a turtle community? This experience will scare your wits out. But trust me, with professionals like diving the crab, there is nothing to worry about.
+
+            Diving the crab is a diving club that offers scuba diving lessons and other sea sports packages. The club is a conglomerate of experts who will guide you in snorkeling, scuba lessons, lagoon kayaking, and windsurfing.</p>
+        
+        <h1>Playing Golf</h1>
         <img id="img1" src="https://i.pinimg.com/236x/85/63/17/856317b3f208de8ecf37c85840747285.jpg" alt="" />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.</p>
-       
-        <h1>Wrap Text</h1>
+        <p>After all your beach activities, it is now time to roll the golf ball in the lush gardens of the Leisure Lodge Golf Club. The lodge boasts of a world-class golf course and is among the best golf clubs in East Africa.
+
+            The fairways run are fully irrigated and traverse Cashewnut, Casuarina, and Pine trees, providing an exciting, yet challenging ground to play your game.
+
+            The course has 85 bunkers, a big lake, and stretches 6084 meters.  While on the course, you will enjoy the presence of different birds and monkeys. It is a perfect place to spend a holiday with your whole family or as a group of golfers</p>
+        
+         <h1> Diani Beach Art Gallery Exhibitions</h1>
         <img id="img2" src="https://i.pinimg.com/236x/85/63/17/856317b3f208de8ecf37c85840747285.jpg" alt=""/>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.</p>
+        <p>ow can you leave Kenya without seeing, touching, and even purchasing some of our art items?
+
+Diani Beach Art Gallery is a hub of some of Africa’s most beautiful art pieces.
+
+This gallery brings together unique and unusual pieces of art made by talented Africans, at meager prices.
+
+The entrance is endowed with superb sculptures, and all workers have a knack for art and creation. Here, you will find some beautiful creations that you may never see in any other part of the world.
+
+The place is open from Monday up to Saturday between 9 am, and 5 pm, and no prior bookings are needed.  You can follow the Diani beach Art Gallery on Twitter or join their Facebook group, and see what to expect during your visit.</p>
     </div>
     </div>     
 
