ceaa15a7fc7840919cbfc228f87b57d23b84692b
Zamzam <Zamzamhassan44@gmail.com>
2022-12-22 22:44:45 +0300

    used useRoutes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom"
+import { useRoutes } from "react-router-dom";
+
 import Home from "./components/Home/Home"
 import Hero from "./components/Hero/Hero"
 import AboutUs from "./components/AboutUs"
 import Lamu from "./components/Lamu/Lamu"
-import Destination from "./components/Destination"
 import Content from "./components/Content"
 import Wildlife from "./components/Wildlife/Wildlife"
 import Coastal from "./components/Coastal/Coastal"
@@ -20,12 +21,31 @@ import Parks from "./components/Parks/Parks"
 
 
 function App() {
+
+  const routes = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: 'about', element: <AboutUs/> } 
+    { path: 'hero', element: <Hero/> } 
+    { path: 'content', element: <Content/> } 
+    { path: 'lamu', element: <Lamu/> } 
+    { path: 'mombasa', element: <Mombasa/> } 
+    { path: 'malindi', element: <Malindi/> } 
+    { path: 'mtwapa', element: <Mtwapa/> } 
+    { path: 'diani', element: <Diani/> } 
+    { path: 'parks', element: <Content/> } 
+    <Route path="wildlife" element={ <Wildlife/> } 
+    <Route path="coast" element={ <Coastal/> } 
+    <Route path="north" element={ <North/> } 
+    <Route path="nairobi" element={ <Nairobi/> } 
+    <Route path="culture" element={ <Culture/> } 
+]);
+
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={ <Home/> } />
         <Route path="about" element={ <AboutUs/> } />
-        <Route path="destination" element={ <Destination/> } />
         <Route path="hero" element={ <Hero/> } />
         <Route path="content" element={ <Content/> } />
         <Route path="lamu" element={ <Lamu/> } />
