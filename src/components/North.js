ac374d08e4077481771fa60b1bdaa5c1cdc6239f
Zamzam <Zamzamhassan44@gmail.com>
2023-02-01 23:57:47 +0300

    added footer to northern.js

--- src/components/North.js
+++ src/components/North.js
@@ -1,4 +1,4 @@
-
+import Footer from "./Footer/Footer";
 const North = () => {
     return ( 
        <div className='container'>
@@ -27,6 +27,7 @@ const North = () => {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.</p>
     
     </div>
+    <Footer />
     </div>     
 
          );
