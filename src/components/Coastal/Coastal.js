62fc2cb1a948fa074d636c5672e0c5517723da92
Zamzam <Zamzamhassan44@gmail.com>
2023-02-02 01:12:46 +0300

    refactor

--- src/components/Coastal/Coastal.js
+++ src/components/Coastal/Coastal.js
@@ -17,17 +17,16 @@ const Coastal = () => {
     <div class="card_item">
     <div class="card_iner">
         <div class="card_top">
-        {/* <img src="https://i.pinimg.com/564x/d4/24/32/d424323fbcc9ef59f64c161119c07ef5.jpg" alt="car" /> */}
+        <img src="https://i.pinimg.com/564x/23/90/df/2390df7c4e5d8047b19dfc2f2a6e2a7c.jpg" alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
               Lamu
           </div>
-          <div class="card_info">
-            <p class="title">Historic touristic town at North coast</p>
-            <p>
-            Lamu is vastly different from most cities in Kenya, most particularly due to the almost complete lack of motor vehicles and corresponding traffic.             </p>
-          </div>
+          {/* <div class="card_info"> */}
+            {/* <p class="title">Historic touristic town at North coast</p> */}
+            
+          {/* </div> */}
           <div class="card_creator">
           {/* <h1>Excited to learn more about Lamu?</h1> */}
 
@@ -45,17 +44,17 @@ const Coastal = () => {
     <div class="card_item">
       <div class="card_iner">
         <div class="card_top">
-        {/* <img src="https://i.pinimg.com/564x/bb/0f/22/bb0f221f13094a4aa631abf7eff90ed5.jpg" alt="car" /> */}
+        <img src="https://i.pinimg.com/564x/d1/2e/9a/d12e9a44a172f1fab94ea638ccea67db.jpg" alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
               Mombasa
           </div>
-          <div class="card_info">
+          {/* <div class="card_info">
             <p class="title">Beautiful beaches and colonial heritage</p>
             <p>
             Mombasa offers a diverse marine life, world-class hotels and friendly atmosphere. There is a tropical climate is a great destination filled with activities for all ages.</p>
-          </div>
+          </div> */}
           <div class="card_creator">
           {/* <h1>Excited to learn more about Mombasa?</h1> */}
 
@@ -71,17 +70,17 @@ const Coastal = () => {
     <div class="card_item">
     <div class="card_iner">
         <div class="card_top">
-        {/* <img src="https://www.travelmoran.com/wp-content/uploads/2022/06/vasco-da-gama-pillar-1024x1024.jpg" alt="car" /> */}
+        <img src="https://miro.medium.com/max/500/1*Hsxd1iWcMPWp4XYHqC0p6g.jpeg" alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
               Malindi
           </div>
-          <div class="card_info">
+          {/* <div class="card_info">
             <p class="title"> The most scenic beaches </p>
             <p>
             Lamu is vastly different from most cities in Kenya, most particularly due to the almost complete lack of motor vehicles and corresponding traffic.             </p>
-          </div>
+          </div> */}
           <div class="card_creator">
           {/* <h1>Excited to learn more about Malindi?</h1> */}
 
@@ -97,17 +96,17 @@ const Coastal = () => {
     <div class="card_item">
     <div class="card_iner">
         <div class="card_top">
-        {/* <img src="https://i.pinimg.com/236x/7c/1e/6a/7c1e6a1c781afc72ae8ac9ed98ab1d2a.jpg" alt="car" /> */}
+        <img src="https://thumbs.dreamstime.com/b/old-abandoned-building-ocean-shore-old-abandoned-building-ocean-shore-mtwapa-mombasa-173320563.jpg" alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
               Mtwapa
           </div>
-          <div class="card_info">
+          {/* <div class="card_info">
             <p class="title">Residents are Europeans</p>
             <p>
             Lamu is vastly different from most cities in Kenya, most particularly due to the almost complete lack of motor vehicles and corresponding traffic.             </p>
-          </div>
+          </div> */}
           <div class="card_creator">
           {/* <h1>Excited to learn more about Mtwapa?</h1> */}
 
@@ -123,17 +122,17 @@ const Coastal = () => {
     <div class="card_item">
     <div class="card_iner">
         <div class="card_top">
-        {/* <img src="https://i.pinimg.com/236x/7c/1e/6a/7c1e6a1c781afc72ae8ac9ed98ab1d2a.jpg" alt="car" /> */}
+        <img src="https://www.tesoroturismo.it/wp-content/uploads/2022/07/diani-beach-spiaggia.jpeg" alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
               Diani Beach
           </div>
-          <div class="card_info">
+          {/* <div class="card_info">
             <p class="title">A resort area on the Indian Ocean in Kenya</p>
             <p>
             Lamu is vastly different from most cities in Kenya, most particularly due to the almost complete lack of motor vehicles and corresponding traffic.             </p>
-          </div>
+          </div> */}
           <div class="card_creator">
           {/* <h1>Excited to learn more about Diani Beach?</h1> */}
 
