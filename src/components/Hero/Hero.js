01a1c8b51280c361d97e0bbe861b9bede7801de0
Zamzam <Zamzamhassan44@gmail.com>
2023-02-02 17:45:49 +0300

    passed images to hero's page as props instead of external links

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,7 +4,14 @@ import { NavLink } from "react-router-dom"
 import Footer from '../Footer/Footer';
 // import { Tooltip } from 'bootstrap';
 
-
+import Lion from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Lion.jpg'
+import Coastal from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Coastal.jpg'
+import North from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/North.jpg'
+import Safaris from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Safaris.jpg'
+import Nairobi from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Nairobi.jpg'
+import Culture from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Culture.jpg'
+import Parks from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Parks.jpg'
+import Features from '/home/zamzam/Desktop/Development/PostSchoolProjects/magical-kenya/src/components/Hero/images/Features.jpg'
 
 function Hero() {
     return (
@@ -24,7 +31,8 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/c5/33/47/c53347580119d787b0a08f669cfa1761.jpg" alt="car" />
+          <img src={Lion} />
+        {/* <img src="https://i.pinimg.com/564x/c5/33/47/c53347580119d787b0a08f669cfa1761.jpg" alt="car" /> */}
         </div>
         <div class="card_bottom">
           <div class="card_category">
@@ -42,7 +50,8 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-          <img src="https://i.pinimg.com/564x/c4/b5/f3/c4b5f354da2fd2a4ad62f9b50e71be6c.jpg" alt="car" />
+          <img src={Coastal} />
+          {/* <img src="https://i.pinimg.com/564x/c4/b5/f3/c4b5f354da2fd2a4ad62f9b50e71be6c.jpg" alt="car" /> */}
         </div>
         <div class="card_bottom">
           <div class="card_category">
@@ -60,7 +69,9 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/18/79/83/187983487c2a6f1b4cd7fe2a809e9649.jpg" alt="car" />
+        <img src={North}  alt="" />
+
+        {/* // https://i.pinimg.com/564x/18/79/83/187983487c2a6f1b4cd7fe2a809e9649.jpg */}
         </div>
         <div class="card_bottom">
           <div class="card_category">
@@ -70,43 +81,29 @@ function Hero() {
           <div class="card_creator">
             <NavLink to='/north'>
             <button className='button'>Explore More</button>
-            </NavLink>          </div>
+            </NavLink>         
+             </div>
         </div>
       </div>
     </div>
+
+   
+
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/45/48/c6/4548c6d44cebd1917667a74ea7df4ed5.jpg" alt="car" />
-        </div>
-        <div class="card_bottom">
-          <div class="card_category">
-            Nairobi city
-          </div>
-     \
-          <div class="card_creator">
-            <NavLink to='/nairobi'>
-            <button className='button'>Explore More</button>
-            </NavLink>          </div>
-        </div>
-      </div>
-    </div>
-    <div class="card_item">
-      <div class="card_inner">
-        <div class="card_top">
-        <img src="https://i.pinimg.com/564x/f6/71/d1/f671d17b6393a922e4fe959cec5f9a23.jpg" alt="car" />
+        <img src={Safaris}
+        // "https://i.pinimg.com/564x/f6/71/d1/f671d17b6393a922e4fe959cec5f9a23.jpg" 
+        alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
             safaris
           </div>
-         
           <div class="card_creator">
-            
             <NavLink to='/safari'>
             <button className='button'>Explore More</button>
             </NavLink>
-
           </div>
         </div>
       </div>
@@ -114,13 +111,34 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/a5/13/1f/a5131f82b596703bb90c553cfdd12bb2.jpg" alt="car" />
+        <img src={Nairobi}
+        // "https://i.pinimg.com/564x/45/48/c6/4548c6d44cebd1917667a74ea7df4ed5.jpg"
+         alt="car" />
+        </div>
+        <div class="card_bottom">
+          <div class="card_category">
+            Nairobi
+          </div>
+          <div class="card_creator">
+            <NavLink to='/nairobi'>
+            <button className='button'>Explore More</button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card_item">
+      <div class="card_inner">
+        <div class="card_top">
+        <img src={Culture}
+        // "https://i.pinimg.com/564x/a5/13/1f/a5131f82b596703bb90c553cfdd12bb2.jpg"
+         alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
             Diverse Culture
           </div>
-        
           <div class="card_creator">
             <NavLink to='/culture'>
             <button className='button'>Explore More</button>
@@ -133,7 +151,9 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/e1/b9/0d/e1b90da353281892312a53585a3a1cc0.jpg" alt="car" />
+        <img src={Parks}
+        // "https://i.pinimg.com/564x/e1/b9/0d/e1b90da353281892312a53585a3a1cc0.jpg"
+         alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
@@ -152,7 +172,9 @@ function Hero() {
     <div class="card_item">
       <div class="card_inner">
         <div class="card_top">
-        <img src="https://i.pinimg.com/564x/68/64/d0/6864d0ed10b64e5d784ebd7f859602ff.jpg" alt="car" />
+        <img src={Features}
+        // "https://i.pinimg.com/564x/68/64/d0/6864d0ed10b64e5d784ebd7f859602ff.jpg"
+         alt="car" />
         </div>
         <div class="card_bottom">
           <div class="card_category">
