9171055abb450140f04109b2e012dbe02e0350db
Zamzam <Zamzamhassan44@gmail.com>
2023-02-02 17:53:47 +0300

    footer.js refactor

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,12 +12,14 @@ const Footer = () => {
 
           <div class="social">
             <h3>Follow us</h3>
-            <a href="#"><i class="fa fa-facebook"></i> </a>
+            {/* <a href="#"><i class="fa fa-facebook"></i> </a> */}
             <a href="https://twitter.com/ZamuHajji"><i class="fa fa-twitter"></i> </a>
-            <a href="#"><i class="fa fa-snapchat-ghost"></i> </a>
+            {/* <a href="#"><i class="fa fa-snapchat-ghost"></i> </a> */}
             <a href="https://www.instagram.com/zamuhajji/?hl=el"><i class="fa fa-instagram"></i> </a>
             <a href="#"><i class="fa fa-whatsapp"></i> </a>
             <a href="https://www.linkedin.com/in/zamzam-hassan-16a7557a/"><i class="fa fa-linkedin"></i> </a>
+            <a href="https://github.com/MissZamzam"><i class="fa fa-github"></i> </a>
+
           </div> 
       </div>
       <div className='footer-bottom'>
