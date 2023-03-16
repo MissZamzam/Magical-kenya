ccf13a156627b6072e6291480ca7ef042c3036b9
Zamzam <Zamzamhassan44@gmail.com>
2023-03-17 00:52:47 +0300

    wrapped anchor over images in wildlife.js

--- src/components/Wildlife/Wildlife.js
+++ src/components/Wildlife/Wildlife.js
@@ -51,79 +51,106 @@ const Wildlife = () => {
         </p>
 
         <h1>Black rhinoceros</h1>
-        
-        <img id="img1" src="https://i.pinimg.com/236x/55/5f/30/555f300947390c51bca21a68262a74d5.jpg" alt="" />
+                <a href="https://www.expertafrica.com/wildlife/black-rhino/kenya">
+                    <img id="img1" src="https://i.pinimg.com/236x/55/5f/30/555f300947390c51bca21a68262a74d5.jpg" alt="" />
+                </a>
        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
         <br></br>
         <br></br>
-        <a href="https://www.expertafrica.com/wildlife/black-rhino/kenya">Black Rhino sightings in Kenya</a>
 
        </p>
 
         <h1>Leopard</h1>
-        <img id="img2" src="https://i.pinimg.com/236x/8f/ca/29/8fca292a87dfe712f11225cc1dc3498d.jpg" alt=""/>
+                   <a href="https://www.expertafrica.com/wildlife/leopard/kenya">
+                            <img id="img2" src="https://i.pinimg.com/236x/8f/ca/29/8fca292a87dfe712f11225cc1dc3498d.jpg" alt=""/>
+
+                   </a>
+
+       
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
             <br></br>
             <br></br>
-            <a href="https://www.expertafrica.com/wildlife/leopard/kenya">Leopard sightings in Kenya</a>
 
         </p>
 
         <h1>Zebra</h1>
-        <img id="img1" src="https://i.pinimg.com/564x/76/46/6e/76466ea092ecc98e59612c951ad17e0e.jpg" alt=""/>
+            <a href="https://www.expertafrica.com/wildlife/zebra/kenya">
+                        <img id="img1" src="https://i.pinimg.com/564x/76/46/6e/76466ea092ecc98e59612c951ad17e0e.jpg" alt=""/>
+
+            </a>
+
+        
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
             <br></br>
             <br></br>
-            <a href="https://www.expertafrica.com/wildlife/zebra/kenya">Zebra sightings in Kenya</a>
 
         </p>
 
    
         <h1>Gerenuk</h1>
-        <img id="img2" src="https://www.awf.org/sites/default/files/styles/vertical_image/public/2020-04/Website_SpeciesPage_Gerenuk03_01_Solutions.jpg?h=3c6d87fa&itok=OSavf8oA" alt="" />
+            <a href="https://www.expertafrica.com/wildlife/gerenuk/kenya">
+                        <img id="img2" src="https://www.awf.org/sites/default/files/styles/vertical_image/public/2020-04/Website_SpeciesPage_Gerenuk03_01_Solutions.jpg?h=3c6d87fa&itok=OSavf8oA" alt="" />
+
+            </a>
+
+       
         <p>The gerenuk, whose name means “giraffe-necked” in Somali, is an exceptionally long-necked antelope. Their head is small for their size, but their eyes and ears are large. Only the males have horns, which are stout and heavily ringed, and they have a more heavily muscled neck than the females do. They have a coat that is brown on the upper back and lighter on the sides. The short tail looks longer as it ends in a tuft of black hair. Like many other gazelles, these large antelopes have preorbital glands in front of the eyes that emit a tar-like, scent-bearing substance they deposit on twigs and bushes to mark their territory. They also have scent glands on their knees that are covered by tufts of hair and between their split hooves.
            They are able to withstand hunting pressures to some degree, which is what has enabled them to survive widely in unprotected regions. However, as human populations grow, they expand settlements, roads, and agriculture, causing in lost or segmented habitats for this species and other wildlife. This makes it more difficult for them to find food and shelter themselves from predators. Due to these threats, populations have declined by 25 percent over the last 14 years (three generations) and is now estimated to be close to meeting the threshold for uplisting to vulnerable.
            <br></br>
             <br></br>
-            <a href="https://www.expertafrica.com/wildlife/gerenuk/kenya">Gerenuk sightings in Kenya</a></p>
+            </p>
 
         
          <h1>Spotted Hyena</h1>
-        <img id="img1" src="https://i.pinimg.com/236x/a0/71/c6/a071c6b2cd30e250f9758c5c80ac39e4.jpg" alt=""/>
+
+                   <a href="https://www.expertafrica.com/wildlife/spotted-hyena/kenya">
+                            <img id="img1" src="https://i.pinimg.com/236x/a0/71/c6/a071c6b2cd30e250f9758c5c80ac39e4.jpg" alt=""/>
+
+                   </a>
+
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
             <br></br>
             <br></br>
-            <a href="https://www.expertafrica.com/wildlife/spotted-hyena/kenya">Spotted Hyena sightings in Kenya</a>
 
         </p>
 
         <h1>Cheetah</h1>
-        <img id="img2" src="https://i.pinimg.com/236x/eb/1b/9c/eb1b9cbb2bf7ee1bf6c40c8f7d139ce7.jpg" alt="" />
+
+        <a href="https://www.expertafrica.com/wildlife/cheetah/kenya">
+                    <img id="img2" src="https://i.pinimg.com/236x/eb/1b/9c/eb1b9cbb2bf7ee1bf6c40c8f7d139ce7.jpg" alt="" />
+
+        </a>
+
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
             <br></br>
             <br></br>
-            <a href="https://www.expertafrica.com/wildlife/cheetah/kenya">Cheetah sightings in Kenya</a>
 
         </p>
 
         <h1>Birds of Kenya</h1>
-        <img id="img1" src="https://www.birdspot.co.uk/wp-content/uploads/2020/01/grey-crowned-crane.jpg" alt=""/>
+
+                    <a href="https://www.tripsavvy.com/birds-of-kenya-4123320">
+                                <img id="img1" src="https://www.birdspot.co.uk/wp-content/uploads/2020/01/grey-crowned-crane.jpg" alt=""/>
+
+                    </a>
+
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium expedita facere doloribus sint illo, minus, explicabo eius quam eaque eos error est nulla. Facilis ratione nesciunt fugiat ipsa, aut quam odio eaque architecto corrupti sapiente consectetur laborum possimus culpa voluptate. Iure in placeat voluptate nihil quam voluptatem asperiores omnis, aliquid reiciendis, aliquam praesentium, ab beatae. Voluptate voluptas dolor earum, fugit aperiam totam ipsam incidunt animi aliquam? Atque ut expedita iure culpa explicabo cupiditate laborum reiciendis doloribus repellat recusandae. At sunt, iusto consequatur cumque veritatis debitis alias incidunt esse? Labore asperiores alias deleniti consequatur dolores quidem sit accusantium nemo earum minus non reprehenderit, necessitatibus nam. Enim, aspernatur, veritatis sunt quia expedita assumenda vitae pariatur itaque eveniet ad dignissimos? Voluptatum, neque. Consequuntur perferendis placeat quis earum, officiis repellat, fugit reprehenderit aut iusto minus nemo quas sed quia quae dolore neque cupiditate, expedita distinctio. Asperiores obcaecati, quidem facilis beatae accusamus aliquid enim veritatis harum, tempore autem magni sint modi voluptates perspiciatis itaque voluptate voluptatem! Tempora quia ad vero quos in ex, id doloribus labore totam cupiditate alias. Magnam minima nemo deleniti provident iure dolorem ipsa maxime architecto. Consequatur distinctio, illum fugiat suscipit repellendus exercitationem, dolorum voluptatum rerum tenetur dignissimos repellat officiis nostrum velit.
             <br></br>
             <br></br>
-            <a href="https://www.tripsavvy.com/birds-of-kenya-4123320">Birds of Kenya, Africa</a>
 
         </p>
 
 
         <h1>Wildebeest Migration</h1>
-        <img id="img2" src="https://i.pinimg.com/564x/6c/34/47/6c34471717f1c4a4948967c04cf38842.jpg" alt="" />
+        <a href="https://www.africanmeccasafaris.com/travel-guide/kenya/parks-reserves/masai-mara/wildebeest-migration">
+                    <img id="img2" src="https://i.pinimg.com/564x/6c/34/47/6c34471717f1c4a4948967c04cf38842.jpg" alt="" />
+
+        </a>
+
+       
         <p>The Great Migration as it is known is the movement of a million plus wildebeest from Serengeti in Tanzania northwards into the adjoining Masai Mara Reserve in Kenya. It is one of the world's most spectacular and thrilling display of wildlife behaviour. The migration takes place every year with the animals primal instincts guiding them towards greener pastures following the climatic rain fall patterns over the Serengeti-Maasai Mara eco system. For the 2021 and 2022 seasons we recommend planning your travel for mid July as this is when the migration typically starts proper with the larger herds of wildebeest starting the move into Masai Mara. The migration takes place till end of August, before declining into mid September.
 
         Although numbers vary every year, past migrations have seen upto 1.5 million wildebeest, close to a million Zebra and large numbers of others animals undertake the long trek that lasts many weeks. Their journey is especially frought with danger as they cross the crocodile infested Mara and Talek rivers along the way, during which many cannot escape the giant nile crocodiles lying in wait for their prey.Though the migration into Masai Mara typically starts in July and ends late September, the exact dates around this period are not predictable until the first large wildebeest herds actually gather at the northern edge of Serengeti as they near the Mara. These initial herds are sometimes known to gather at a spot for days on end without crossing over to the Mara, so when exactly the final leg of the movement starts is difficult to predict.
-        <br></br>
-        <br></br>
-        <a href="https://www.africanmeccasafaris.com/travel-guide/kenya/parks-reserves/masai-mara/wildebeest-migration">Best Time To See Masai Mara Wildebeest Migration Kenya</a>
 </p>
 
 </div>
