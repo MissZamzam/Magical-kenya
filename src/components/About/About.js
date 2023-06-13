358612cb0b8aec7a4ae283056ee82358ba99c12b
Zamzam <Zamzamhassan44@gmail.com>
2023-06-14 01:39:10 +0300

    refactor

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -92,27 +92,6 @@ function About() {
               </li>
             </ul>
           </section>
-
-          {/* <section class="testimonials">
-    <h3 class="title">Testimonials from our adventurers:</h3>
-    <hr></hr>
-    <p class="quote">Wow! This tour made me realize how much I love being outside with my friends. After going on one of these tours, I can safely say that beer pong is my favorite game all time, also the cultural programs were really interesting!</p>
-    <p class="author">- Albert Herter</p>
-    <p class="quote">Wow, this really blew my mind. We had so much fun at the beach, and also some hidden secrets revealed. Come on, I'm living in this city for 5 years. Amazing!</p>
-    <p class="author">- Sharon Rosenberg</p>
-    <p class="quote">If you want to understand your friends better, head to the mountains. I mean, seriously. It's like sitting next to a campfire and just talk in the sunset, woah. You know? It's like that.</p>
-    <p class="author">- Luis Mendoza</p>
-  </section> */}
-          {/* 
-  <section class="contact">
-    <h3 class="title">Learn more</h3>
-    <p>Want to know about our upcoming events, or come to one of our mixers? Just sign up for our mailing list. No spam from us, we promise! Except for the spam we give you to keep up your energy while you're having fun with your friends. Have a blast! We have tons of that.</p>
-    <hr></hr>
-    <form>
-      <input type="email" placeholder="Email" />
-      <a href="#" class="btn">Subscribe now</a>
-    </form>
-  </section> */}
         </div>
 
         <Footer />
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ function Home() {
             src="https://i.pinimg.com/564x/29/7e/47/297e47215f921f56d9549d32b054c123.jpg"
             className="profile"
           ></img>
-          <h1 className="head">🇰🇪Discover Magical Kenya 🇰🇪</h1>
+          <h1 className="head">Discover Magical Kenya</h1>
         </div>
       </NavLink>
     </div>
